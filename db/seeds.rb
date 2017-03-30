040ebe1fd363f06c6655832f5a2dfb6f21ee3414
Johann Ramos <johann.ramos.r@gmail.com>
2017-03-30 16:55:24 -0300

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Company.destroy_all
+Company.create!([{
+                   name: "Microsoft",
+                   address: "123 Road",
+                   city: "Santiago",
+                   country: "Chile"
+               },
+               {
+                   name: "Apple",
+                   address: "123 Road",
+                   city: "Santiago",
+                   country: "Chile"
+               },
+               {
+                     name: "Tesla",
+                     address: "123 Road",
+                     city: "Santiago",
+                     country: "Chile"
+               }])
+
+p "Created #{Company.count} companies"
+
+
+Owner.destroy_all
+Owner.create!([{
+                     name: "Bill Gates"
+                 },
+                 {
+                     name: "Steve Jobs"
+                 },
+                 {
+                     name: "Elon Musk"
+                 }])
+
+p "Created #{Owner.count} owners"
